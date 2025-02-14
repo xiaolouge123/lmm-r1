05f3cae2d34ffb6f54c55c64d0193713fe203548
xiaolouge <davidzhang1107@gmail.com>
2025-02-14 21:07:28 +0800

    add more

---
+++ examples/scripts/convert_dataset/utils.py
@@ -0,0 +1,150 @@
+from string import Template
+
+ACTION_SPACE = """
+<action>
+    <action_name>
+        SWIPE
+    </action_name>
+    <description>
+        Swipe the screen in one direction
+    </description>
+    <parameters>
+        <parameter>
+            <param_name>
+                direction
+            </param_name>
+            <param_description>
+                the direction to swipe, only left, right, up, down are allowed
+            </param_description>
+        </parameter>
+    </parameters>
+</action>
+<action>
+    <action_name>
+        TAP
+    </action_name>
+    <description>
+        Tap the target element on the screen
+    </description>
+    <parameters>
+        <parameter>
+            <param_name>
+                point
+            </param_name>
+            <param_description>
+                the coordinates of the target element on the screen, format is like [x, y], 
+                x and y are the coordinates of the target element on the screen, range from 0 to 999
+            </param_description>
+        </parameter>
+    </parameters>
+</action>
+<action>
+    <action_name>
+        TYPE
+    </action_name>
+    <description>
+        Type the text in the text input field
+    </description>
+    <parameters>
+        <parameter>
+            <param_name>
+                text
+            </param_name>
+            <param_description>
+                the text string to input
+            </param_description>
+        </parameter>
+    </parameters>
+</action>
+<action>
+    <action_name>
+        TASK_COMPLETE
+    </action_name>
+    <description>
+        The task is completed based on the current state of the screen
+    </description>
+</action>
+<action>
+    <action_name>
+        PRESS_ENTER
+    </action_name>
+    <description>
+        Press the enter key on the keyboard
+    </description>
+</action>
+<action>
+    <action_name>
+        TASK_IMPOSSIBLE
+    </action_name>
+    <description>
+        The task is impossible to be completed based on the current state of the screen   
+    </description>
+</action>
+<action>
+    <action_name>
+        PRESS_BACK
+    </action_name>
+    <description>
+        Press the back key on the keyboard
+    </description>
+</action>
+<action>
+    <action_name>
+        PRESS_HOME
+    </action_name>
+    <description>
+        Press the home key on the keyboard
+    </description>
+</action>
+<action>
+    <action_name>
+        WAIT
+    </action_name>
+    <description>
+        Wait for a while
+    </description>
+</action>
+"""
+
+ACTION_OUTPUT_FORMAT = """
+<action_description>
+    ACTION_DESCRIPTION_FOR_SHORT_IN_NATURAL_LANGUAGE
+</action_description>
+<action>
+    <action_name>
+        ACTION_NAME
+    </action_name>
+    <parameters> // if the action has parameters else null
+        <parameter>
+            <param_name>
+                PARAM_NAME
+            </param_name>
+            <param_value>
+                PARAM_VALUE
+            </param_value>
+        </parameter>
+    </parameters>
+</action>
+"""
+
+QUERY_TEMPLATE = Template(
+    """
+Given the task:
+<task>
+    $instruction
+</task>
+
+Here is the action space you may take:
+<action space>
+    $action_spaces
+</action space>
+
+The output action format likes this:
+<output action format>
+    $output_action_format
+</output action format>
+
+Please think step by step to find out the proper action to take. And only output one action at a time.
+Output the thinking process in <think></think> and final answer in <answer> </answer> tags, i.e., <think>your thinking process</think><answer>your action</answer>.
+"""
+)
--- openrlhf/models/remote_rm/action_verifier.py
+++ openrlhf/models/remote_rm/action_verifier.py
@@ -5,10 +5,9 @@ import re
 from argparse import ArgumentParser
 from multiprocessing import Process, Queue
 
-import Levenshtein
+
 from flask import Flask, jsonify, request
-from latex2sympy2_extended import NormalizationConfig
-from math_verify import LatexExtractionConfig, parse, verify
+from utils import action_accuracy_reward as accuracy_reward_func, format_reward as format_reward_func
 
 app = Flask(__name__)
 
@@ -26,65 +25,6 @@ def get_response_from_query(q: str):
     return response.strip()
 
 
-def verify_format(content):
-    return re.match(format_pattern, content, re.DOTALL) is not None
-
-
-def find_similar_problem(problem):
-    max_sim = -1
-    target_problem = None
-    for p in problem_to_answer.keys():
-        sim = Levenshtein.ratio(problem, p)
-        if sim > max_sim:
-            max_sim = sim
-            target_problem = p
-    return target_problem
-
-
-def verify_action(input_queue, output_queue):
-    while True:
-        content, sol = input_queue.get()
-        gold_parsed = parse(
-            sol,
-            extraction_mode="first_match",
-            extraction_config=[LatexExtractionConfig()],
-        )
-        if len(gold_parsed) != 0:
-            # We require the answer to be provided in correct latex (no malformed operators)
-            answer_parsed = parse(
-                content,
-                extraction_config=[
-                    LatexExtractionConfig(
-                        normalization_config=NormalizationConfig(
-                            nits=False,
-                            malformed_operators=False,
-                            basic_latex=True,
-                            equations=True,
-                            boxed=True,
-                            units=True,
-                        ),
-                        # Ensures that boxed is tried first
-                        boxed_match_priority=0,
-                        try_extract_without_anchor=False,
-                    )
-                ],
-                extraction_mode="first_match",
-            )
-            # Reward 1 if the content is the same as the ground truth, 0 otherwise
-            print(answer_parsed, gold_parsed)
-            try:
-                reward = float(verify(answer_parsed, gold_parsed))
-            except Exception as e:
-                reward = 1.0
-                print("Failed to verify: ", e)
-        else:
-            # If the gold solution is not parseable, we reward 1 to skip this example
-            reward = 1.0
-            print("Failed to parse gold solution: ", sol)
-
-        output_queue.put(reward)
-
-
 @app.route("/get_reward", methods=["POST"])
 def get_reward():
     # 获取请求中的 JSON 数据
@@ -93,77 +33,63 @@ def get_reward():
     if "query" not in data:
         return jsonify({"error": "queries field is required"}), 400
     rewards = []
-    for q,problem in zip(data["query"],data["prompts"]):
+    for q, problem in zip(data["query"], data["prompts"]):
         if problem is None:
             return jsonify({"error": f"problem not found from {q}"}), 400
         if problem not in problem_to_answer:
             # This should not happen
             print(f"problem not exists: {problem}")
-            problem = find_similar_problem(problem)
+            rewards.append(0.0)
+            continue
         answer = problem_to_answer[problem]
         response = get_response_from_query(q) or q
         if response is None:
             return jsonify({"error": f"response not found from {q}"}), 400
-        format_reward = float(verify_format(response))
-        input_queue.put((response, answer))
-        acc_reward = float(output_queue.get())
+
+        format_reward = float(format_reward_func(response))
+        acc_reward = float(accuracy_reward_func(response, answer))
         do_print = random.randint(1, 20) == 1
         if do_print:
-            info=f"Query: {q}\n\nProblem: {problem}\n\n Answer: {answer}\n\n Response: {response}\n\n Format Reward: {format_reward}\n\n Acc Reward: {acc_reward}\n\n"
-            info = re.sub(r"<\|.*?\|>","",info)
+            info = f"Query: {q}\n\nProblem: {problem}\n\n Answer: {answer}\n\n Response: {response}\n\n Format Reward: {format_reward}\n\n Acc Reward: {acc_reward}\n\n"
+            info = re.sub(r"<\|.*?\|>", "", info)
             print(info)
-            
-        rewards.append(0.5 * format_reward + acc_reward)
+
+        rewards.append(0.5 * format_reward + 0.5 * acc_reward)
     # 返回包含 rewards 的响应
     return jsonify({"rewards": rewards})
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument(
-        "--dataset", type=str, default="math_dataset", help="Dataset to use"
-    )
-    parser.add_argument(
-        "--prompt-template", type=str, default="chatml", help="Prompt template"
-    )
-    parser.add_argument(
-        "--input_key", type=str, default="prompt", help="The key name of prompt."
-    )
+    parser.add_argument("--dataset", type=str, default="math_dataset", help="Dataset to use")
+    parser.add_argument("--prompt-template", type=str, default="chatml", help="Prompt template")
+    parser.add_argument("--input_key", type=str, default="prompt", help="The key name of prompt.")
     args = parser.parse_args()
+
     if args.dataset.endswith("json"):
         with open(args.dataset, "r") as f:
             dataset = json.load(f)
     elif args.dataset.endswith("jsonl"):
         with open(args.dataset, "r") as f:
             dataset = [json.loads(l) for l in f.readlines()]
+    else:
+        raise ValueError(f"Unknown dataset format: {args.dataset}")
 
-    format_pattern = r"^<think>.*?</think><answer>.*?</answer>$"
+    for item in dataset:
+        problem = item[args.input_key]
+        answer = item["answer"].strip()
+        problem_to_answer[problem] = answer
 
-    if args.prompt_template=="chatml":
+    if args.prompt_template == "chatml":
         problem_pattern = r"<\|im_start\|>user\n(.*?)<\|im_end\|>"
         response_prefix = r"<\|im_start\|>assistant\n"
-    elif args.prompt_template=="qwen1":
+    elif args.prompt_template == "qwen1":
         problem_pattern = r"｜User｜>(.*?)<｜Assistant｜>"
         response_prefix = r"<｜Assistant｜>"
-    elif args.prompt_template=="base":
+    elif args.prompt_template == "base":
         problem_pattern = r"User: (.*?)\n\nAssistant:"
         response_prefix = r"Assistant: "
     else:
         raise ValueError(f"Unknown chat format: {args.dataset}")
-    print("load dataset success")
-    for item in dataset:
-        problem = item[args.input_key]
-        answer = item["answer"].strip()
-        # we require the answer to be in latex format
-        if answer[0] != "$":
-            answer = "$" + answer + "$"
-        problem_to_answer[problem] = answer
-
-    # math_verify can only run in main thread
-    input_queue = Queue()
-    output_queue = Queue()
-    p = Process(target=verify_math, args=(input_queue, output_queue))
-    p.start()
 
     app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
-    p.kill()
